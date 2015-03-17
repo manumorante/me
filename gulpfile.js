9e015e558b477deae90572d1b55c1155c298f86c
Manu Morante <manuel.morante@razorfishhealthware.com>
2015-03-17 18:45:49 +0100

    Polaroids media queries

--- gulpfile.js
+++ gulpfile.js
@@ -1,70 +1,62 @@
 var _             = require('gulp'),
-  watch           = require('gulp-watch'),
-  concat          = require('gulp-concat'),
-  connect         = require('gulp-connect'),
-  compass         = require('gulp-compass'),
-  uglify          = require('gulp-uglify'),
-  del             = require('del'),
-  include         = require('gulp-include'),
+    watch           = require('gulp-watch'),
+    connect         = require('gulp-connect'),
+    compass         = require('gulp-compass'),
+    uglify          = require('gulp-uglify'),
+    del             = require('del'),
+    include         = require('gulp-include'),
+
+    c = {
+      source: {
+        dir:        'source',
+        assets:     'source/assets',
+        js:         'source/assets/js',
+        sass:       'source/assets/sass',
+        images:     'source/assets/images',
+        fonts:      'source/assets/fonts'
+      },
+      build: {
+        dir:        'build',
+        assets:     'build/assets',
+        js:         'build/assets/js',
+        css:        'build/assets/css',
+        images:     'build/assets/images',
+        fonts:      'build/assets/fonts'
+      }
+    };
 
 
-  c = {
-    source: {
-      dir:          'source',
-      assets:       'source/assets',
-      js:           'source/assets/js',
-      sass:         'source/assets/sass',
-      images:       'source/assets/images',
-      fonts:        'source/assets/fonts'
-    },
-    build: {
-      dir:          'build',
-      assets:       'build/assets',
-      js:           'build/assets/js',
-      css:          'build/assets/css',
-      images:       'build/assets/images',
-      fonts:        'build/assets/fonts'
-    }
-  };
-
-// HTML
 _.task('html', function(){
   _.src(c.source.dir +'/**/*.html').pipe(_.dest(c.build.dir));
 });
 
 
-// Images
 _.task('images', function(){
   _.src(c.source.images + '/*.*').pipe(_.dest(c.build.images));
 });
 
 
-// Fonts
 _.task('fonts', function(){
   _.src(c.source.fonts +'/**/*.*').pipe(_.dest(c.build.fonts));
 });
 
 
-// CSS
 _.task('styles', function(){
   _.src(c.source.sass +'/application.scss')
-    .pipe(compass({config_file: c.source.dir +'/config/compass.rb', sass: c.source.sass, css: c.build.css}))
+    .pipe(compass({config_file: c.source.dir +'/compass.rb', sass: c.source.sass, css: c.build.css}))
 });
 
 
-//  Javascript
 _.task('scripts', function(){
   _.src(c.source.js + '/application.js')
     .pipe( include() )
     .pipe( uglify({ mangle: false }) )
     .pipe( _.dest(c.build.js) );
 
-  // Sourcempas
   _.src(c.source.js +'/lib/*.js.map').pipe(_.dest(c.build.js));
 });
 
 
-// Watch
 _.task('watch', function(){
   _.watch(c.source.sass     +'/**/*.scss',    ['styles']);
   _.watch(c.source.images   +'/*.*',          ['images']);
@@ -73,13 +65,11 @@ _.task('watch', function(){
 });
 
 
-// Server
-_.task('connect', function(){
+_.task('server', function(){
   connect.server({root: c.build.dir, port: 3000});
 });
 
 
-// Build w/ clean
 _.task('build:clean', function(){
   del( c.build.dir, function(){
     _.start('build');
@@ -87,12 +77,10 @@ _.task('build:clean', function(){
 });
 
 
-// Build
 _.task('build', ['fonts', 'html', 'scripts', 'images', 'styles']);
 
 
-// Default
 _.task('default', ['build:clean'], function(){
-  _.start('connect');
+  _.start('server');
   _.start('watch');
 });
